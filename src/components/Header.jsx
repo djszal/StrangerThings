18f878790b4cd3af42112a447a9c8c77fb5d6260
Maria Tran <mp.tran828@gmail.com>
2022-12-11 16:04:48 -0600

    hi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,15 +9,21 @@ import AllPosts from "./AllPosts";
 
 const Header = () => {
 
+        const[newToken, setToken] = useState(localStorage.getItem("token"));
+
+        const Logout = () => {
+            localStorage.removeItem("token");
+        }
     return (
         <div className="header">
             <h3>Stranger's Things</h3>
             <div className="navbar">
                 <Link to={'/'} className="nav-links">Posts</Link>
                 <Link to={'/profile'} className="nav-links">Profile</Link>
-
-                <Link to={'/login'} className="nav-links">Login</Link>
-                <Link to={'/'} className="nav-links">Logout</Link>
+                {!newToken ? (
+                <Link to={'/login'} className="nav-links">Login</Link> ) :
+                (<Link to={'/'} className="nav-links" onClickCapture={Logout}>Logout</Link>
+                )}                
                 <Link to={'/newpost'} className="nav-links">New Posts</Link>
             </div>
 
